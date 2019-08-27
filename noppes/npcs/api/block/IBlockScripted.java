d5e731b91dc69a680e4f1b931cb402c787207b1c
Rain_Effect <i@mc-rain.cn>
2019-08-27 23:36:27 +0800

    更新IBlockScripted汉化
    更新noppes.npcs.api.block.IBlockScripted汉化

--- noppes/npcs/api/block/IBlockScripted.java
+++ noppes/npcs/api/block/IBlockScripted.java
@@ -6,104 +6,180 @@ import noppes.npcs.api.item.IItemStack;
 public interface IBlockScripted extends IBlock{
 
 	/**
-	 * @param item The item to be set as model
+	 * 设置当前方块的模型（物品堆方式）
+	 * @param item 物品堆
 	 */
 	public void setModel(IItemStack item);
 
+	/**
+	 * 设置当前方块的模型（物品名方式）
+	 * @param name 物品名
+	 */
 	public void setModel(String name);
 
+	/**
+	 * 返回当前方块的模型所对应的物品堆
+	 */
 	public IItemStack getModel();
 	
+	/**
+	 * 返回当前方块的计时器组
+	 */
 	public ITimers getTimers();
 
 	/**
-	 * @param strength Sets the strength of the redstone signal (0-15)
+	 * 设置当前方块发出的红石信号强度
+	 * @param strength 红石信号强度（0-15）
 	 */
 	public void setRedstonePower(int strength);
 
 	/**
-	 * @return Returns the current redstone power (0-15) this block is giving off
+	 * 返回当前方块发出的红石信号强度
+	 * @return 红石信号强度（0-15）
 	 */
 	public int getRedstonePower();
 
+	/**
+	 * 设置当前方块可否被攀爬
+	 */
 	public void setIsLadder(boolean enabled);
 
+	/**
+	 * 返回当前方块可否被攀爬
+	 */
 	public boolean getIsLadder();
 
 	/**
-	 * @param value Sets the light value (0-15)
+	 * 设置当前方块发出的亮度强度
+	 * @param value 亮度强度（0-15）
 	 */
 	public void setLight(int value);
 
 	/**
-	 * @return Returns the light value (0-15)
+	 * 返回当前方块发出的亮度强度
+	 * @return 亮度强度（0-15）
 	 */
 	public int getLight();
 
 	/**
-	 * @param x Scale x (0-10)
-	 * @param y Scale y (0-10)
-	 * @param z Scale z (0-10)
+	 * 设置当前方块的缩放规模
+	 * @param x X轴缩放（0-10）
+	 * @param y Y轴缩放（0-10）
+	 * @param z Z轴缩放（0-10）
 	 */
 	public void setScale(float x, float y, float z);
 
+	/**
+	 * 返回当前方块的X轴缩放规模
+	 */
 	public float getScaleX();
 
+	/**
+	 * 返回当前方块的Y轴缩放规模
+	 */
 	public float getScaleY();
 
+	/**
+	 * 返回当前方块的Z轴缩放规模
+	 */
 	public float getScaleZ();
 
 	/**
-	 * @param x Rotation x (0-359)
-	 * @param y Rotation y (0-359)
-	 * @param z Rotation z (0-359)
+	 * 设置当前方块的旋转角度
+	 * @param x x轴旋转（0-359）
+	 * @param y y轴旋转（0-359）
+	 * @param z z轴旋转（0-359）
 	 */
 	public void setRotation(int x, int y, int z);
 
+	/**
+	 * 返回当前方块的X轴旋转角度
+	 */
 	public int getRotationX();
 
+	/**
+	 * 返回当前方块的Y轴旋转角度
+	 */
 	public int getRotationY();
 
+	/**
+	 * 返回当前方块的Z轴旋转角度
+	 */
 	public int getRotationZ();
 
 	/**
-	 * On servers the enable-command-block option in the server.properties needs to be set to true <br>
-	 * Use /gamerule commandBlockOutput false/true to turn off/on command block feedback <br>
-	 * Setting NpcUseOpCommands to true in the CustomNPCs.cfg should allow the npc to run op commands, be warned this could be a major security risk, use at own risk <br>
-	 * For permission plugins the commands are run under uuid:c9c843f8-4cb1-4c82-aa61-e264291b7bd6 and name:[customnpcs]
-	 * @param command The command to be executed
-	 * @return Returns the commands output
+	 * 在服务器上，server.properties中的enable-command-block选项需要设置为true，<br>
+	 * 使用/gamerule commandBlockOutput false/true以关闭/打开命令反馈<br>
+	 * 在CustomNPCs.cfg中将NpcUseOpCommands设置为true，可允许NPC以OP身份运行命令，但请注意这可能会导致一系列的安全风险，使用风险自负<br>
+	 * 对于权限插件，命令会在该UUID和名称下运行<br>
+	 * UUID:c9c843f8-4cb1-4c82-aa61-e264291b7bd6<br>
+	 * 名称:[customnpcs]<br>
+	 * @param command 要执行的命令
+	 * @return 返回命令输出
 	 */
 	public String executeCommand(String command);
 
+	/**
+	 * 返回当前方块可否被穿透
+	 */
 	public boolean getIsPassible();
 	
+	/**
+	 * 设置当前方块可否被穿透
+	 */
 	public void setIsPassible(boolean bo);
 	
 	/**
-	 * @return Harvesting hardness (-1 makes it unharvestable)
+	 * 返回当前方块的破坏硬度
+	 * @return 破坏硬度（-1为无法破坏）
 	 */
 	public float getHardness();
 	
+	/**
+	 * 设置当前方块的破坏硬度
+	 * @param hardness 破坏硬度（-1为无法破坏）
+	 */
 	public void setHardness(float hardness);
 	
 	/**
-	 * @return Explosion resistance (-1 makes it unexplodable)
+	 * 返回当前方块的爆炸阻力（-1为无法炸毁）
 	 */
 	public float getResistance();
 	
+	/**
+	 * 设置当前方块的爆炸阻力
+	 * @param resistance 爆炸阻力（-1为无法炸毁）
+	 */
 	public void setResistance(float resistance);
 
+	/**
+	 * 获取当前方块的悬浮字对象
+	 */
 	public ITextPlane getTextPlane();
 
+	/**
+	 * 获取当前方块的悬浮字对象
+	 */
 	public ITextPlane getTextPlane2();
 
+	/**
+	 * 获取当前方块的悬浮字对象
+	 */
 	public ITextPlane getTextPlane3();
 
+	/**
+	 * 获取当前方块的悬浮字对象
+	 */
 	public ITextPlane getTextPlane4();
 
+	/**
+	 * 获取当前方块的悬浮字对象
+	 */
 	public ITextPlane getTextPlane5();
 
+	/**
+	 * 获取当前方块的悬浮字对象
+	 */
 	public ITextPlane getTextPlane6();
 
 }
