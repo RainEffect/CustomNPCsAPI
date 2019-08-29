1e0e1c279b24b30a719821740db780bfe7e54c0a
Rain_Effect <i@mc-rain.cn>
2019-08-29 18:31:24 +0800

    更新IBlockFluidContainer汉化和对应doc文档内容
    更新noppes.npcs.api.block.IBlockFluidContainer

--- noppes/npcs/api/block/IBlockFluidContainer.java
+++ noppes/npcs/api/block/IBlockFluidContainer.java
@@ -1,18 +1,33 @@
 package noppes.npcs.api.block;
 
 /**
- * 用于操作在世界内的液体容器方块对象
+ * 用于操作在世界内的流体容器方块对象<br>（尚未进行确定测试，此处翻译可能有误）
  */
 public interface IBlockFluidContainer extends IBlock{
 
+	/**
+	 * 返回当前流体容器方块的已存储流体储量百分比
+	 */
 	public float getFluidPercentage();
 
+	/**
+	 * 返回当前流体容器方块的流体密度
+	 */
 	public float getFuildDensity();
 
+	/**
+	 * 返回当前流体容器方块的流体温度
+	 */
 	public float getFuildTemperature();
 
+	/**
+	 * 返回当前流体容器方块的流体储量
+	 */
 	public float getFluidValue();
 
+	/**
+	 * 返回当前流体容器方块的流体名
+	 */
 	public String getFluidName();
 
 }
